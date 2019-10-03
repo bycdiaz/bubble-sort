14a70b236aa01a9e9c3dc5eebcc99f54e5087f02
Carlos Diaz <cdiaz86@gmail.com>
2019-10-02 20:23:21 -0400

    Adds solution to Bubble Sort By project.

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,5 +1,17 @@
 def bubble_sort_by(array)
 
+  (array.length - 2).times do
+    (array.length - 1).times do |index|
+      if yield(array[index], array[index + 1]) > 0
+        array[index], array[index + 1] = array[index + 1], array[index]
+      end
+    end
+  end
+  array
 end
 
-p bubble_sort_by(["hi","hello","hey"])
+new_array = bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
+
+puts new_array
